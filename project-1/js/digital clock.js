2424293964c38d56829397514173e088c9dcf743
uzairahmed2126 <uzairahmed21261@gmail.com>
2023-07-19 01:58:47 +0530

    digital clock

---
+++ project-1/js/digital clock.js
@@ -0,0 +1,44 @@
+const txthr = document.getElementById('txt-hr')
+const hours = document.getElementById('hours')
+
+const txtmin = document.getElementById('txt-min')
+const minutes = document.getElementById('minutes')
+
+const txtsec = document.getElementById('txt-sec')
+const seconds = document.getElementById('seconds')
+seconds.addEventListener('click',()=>{
+    setTimeout(()=>{
+        txtsec.innerHTML=seconds
+    },1000)
+})
+
+let ampm = document.getElementById('ampm')
+
+// txthr.addEventListener('click',function(){
+//     let time = setTimeout(hours,1000)
+// document.getElementById('hours').innerHTML=time
+// })
+function updateClock() {
+  let h = new Date().getHours()
+  let m = new Date().getMinutes()
+  let s = new Date().getSeconds()
+  ampm='AM'
+  if (h > 12) {
+    h -= 12
+    ampm.innerHTML = 'PM'
+  }
+
+  h = h < 10 ? "0" + h : h
+  m = m < 10 ? "0" + m : m
+  s = s < 10 ? "0" + s : s
+  hours.innerText = h
+  minutes.innerText = m
+  seconds.innerText = s
+  ampm.innerText = ampm
+  setTimeout(()=>{
+    updateClock()
+
+  }, 1000)
+}
+
+updateClock()
