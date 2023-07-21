322c77cf92a07dd3fd05abc78b6aba8059429464
uzairahmed2126 <uzairahmed21261@gmail.com>
2023-07-21 16:20:23 +0530

    Complete

--- project-2/js/Multiplication.js
+++ project-2/js/Multiplication.js
@@ -1,18 +1,41 @@
 const questions = document.getElementById('question')
-const score = document.getElementById('score')
-const submit = document.getElementById('submit')
+const scoreEl = document.getElementById('score')
 const formEl = document.getElementById('form')
-const num1 = Math.ceil(Math.random() * 30)
-const num2 = Math.ceil(Math.random() * 30)
+const num1 = Math.ceil(Math.random() * 1)
+const num2 = Math.ceil(Math.random() * 1)
 const correctAns = num1 * num2
-const text = document.getElementById('text').innerText
+const text = document.getElementById('text')
+const reset = document.getElementById('refresh-btn')
+reset.addEventListener('click',()=>{
+  window.location.reload()
+  let score = JSON.parse(localStorage.getItem('score'))
+scoreEl.innerText = `score : ${0}`
+})
+let score = JSON.parse(localStorage.getItem('score'))
+scoreEl.innerText = `score : ${score}`
 
+if (!score) {
+  score = 0
+}
 questions.innerText = `What is ${num1} multiply by ${num2}?`
 
 formEl.addEventListener('submit', () => {
-  if (correctAns) {
-    score.innerText = `score : ${+1}`
+  const userAns = +text.value
+  if (correctAns === userAns) {
+    score++
+    updatelocalStorage()
   } else {
-    alert('aww')
+    score--
+    updatelocalStorage()
   }
 })
+
+function updatelocalStorage() {
+  localStorage.setItem('score', JSON.stringify(score))
+}
+
+if(window.screen.addEventListener('load',reload)){
+  function reload(){
+scoreEl.innerText = `score : ${0}`
+  }
+}
