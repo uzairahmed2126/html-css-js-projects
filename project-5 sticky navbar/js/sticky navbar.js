1281d51a42d9a0093e5c283551d7c0df3ab20c12
uzairahmed2126 <uzairahmed21261@gmail.com>
2023-07-25 14:32:32 +0530

    sticky navbar

--- project-5 sticky navbar/js/sticky navbar.js
+++ project-5 sticky navbar/js/sticky navbar.js
@@ -1,14 +1,11 @@
 const nav = document.querySelector('.nav-bar')
 const BottomConatiner = document.querySelector('.paragraph')
 
-window.addEventListener('scroll', () => {
-  if (window.scrollY > BottomConatiner.offsetTop-nav.offsetHeight-50) {
+window.addEventListener('scroll', (event) => {
+  console.dir(window.scrollY)
+  if (window.scrollY > 200) {
     nav.classList.add('active')
   } else {
     nav.classList.remove('active')
   }
 })
-
-
-
-
