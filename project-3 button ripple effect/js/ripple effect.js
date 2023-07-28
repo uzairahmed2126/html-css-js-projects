9b1c129dfa4daeae9a6df8e7e11b74f69875e036
uzairahmed2126 <uzairahmed21261@gmail.com>
2023-07-29 02:51:53 +0530

    .;

--- project-3 button ripple effect/js/ripple effect.js
+++
@@ -1,7 +0,0 @@
-const btn = document.querySelector('.btn')
-btn.addEventListener('mouseover', (event) => {
-  const x = event.pageX - btn.offsetLeft
-  const y = event.pageY - btn.offsetTop
-  btn.style.setProperty('--xPos', x + 'px')
-  btn.style.setProperty('--yPos', y + 'px')
-})
