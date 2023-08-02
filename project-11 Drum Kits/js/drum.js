e2c90dca6b2470db8a926d1a7bfa3c60cba52fe8
uzairahmed2126 <uzairahmed21261@gmail.com>
2023-08-02 23:38:44 +0530

    Work audio project

---
+++ project-11 Drum Kits/js/drum.js
@@ -0,0 +1,81 @@
+const container = document.querySelector('.container')
+const SurahFatiha = document.getElementById('btn-1')
+const SurahIkhlas = document.getElementById('btn-2')
+const SurahFalaq = document.getElementById('btn-3')
+const SurahAnNaas = document.getElementById('btn-4')
+const audioEl = document.getElementById('audioEl')
+const span = document.querySelector('span')
+let spanClassPlay;
+let spanClassStop; 
+let isOn = false
+span.addEventListener('click', () => {
+  audioEl.src = './sounds/Surah Al-Fatiha.mp3'
+  span.style.visibility='hidden'
+  isOn = !isOn
+  if (isOn) {
+    audioEl.play()
+     span.innerHTML('class', 'fa-solid fa-pause')
+  } else {
+    audioEl.pause()
+    span.setAttribute('class', 'fa-solid fa-play')
+
+  }
+})
+
+SurahIkhlas.addEventListener('click', () => {
+  audioEl.src = './sounds/Surah Al-Ikhlaas.mp3'
+
+  isOn = !isOn
+  if (isOn) {
+    audioEl.play()
+  } else {
+    audioEl.pause()
+  }
+})
+
+SurahFalaq.addEventListener('click', () => {
+  audioEl.src = './sounds/Surah Al-Falaq.mp3'
+  console.log(isOn)
+
+  isOn = !isOn
+  console.log(isOn)
+  if (isOn) {
+    audioEl.play()
+  } else {
+    audioEl.pause()
+  }
+})
+
+SurahAnNaas.addEventListener('click', () => {
+  audioEl.src = './sounds/Surah An-Naas.mp3'
+  console.log(isOn)
+
+  isOn = !isOn
+  console.log(isOn)
+  if (isOn) {
+    audioEl.play()
+  } else {
+    audioEl.pause()
+  }
+})
+
+// const Kits = [
+//   'Surah Al-Fatiha',
+//   'Surah Al-Ikhlaas',
+//   'Surah Al-Falaq',
+//   'Surah An-Nass',
+// ]
+// const containerEl = document.querySelector('.container')
+
+// Kits.forEach((kit) => {
+//   const btnEl = document.createElement('button')
+//   btnEl.classList.add('btn')
+//   btnEl.innerText = kit
+//   containerEl.appendChild(btnEl)
+//   const audioEl = document.createElement('audio')
+//   audioEl.src='sounds/'+kit+'.mp3'
+//   containerEl.appendChild(audioEl)
+//   btnEl.addEventListener('click',()=>{
+//     audioEl.play()
+//   })
+// })
