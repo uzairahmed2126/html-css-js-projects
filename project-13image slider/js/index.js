83a3668c2cfbbcc4bebec3fcced97998dd75273f
uzairahmed2126 <uzairahmed21261@gmail.com>
2023-08-06 23:40:47 +0530

    image slider

--- project-13image slider/js/index.js
+++ project-13image slider/js/index.js
@@ -1,13 +1,33 @@
 const prev = document.querySelector('.prev')
 const next = document.querySelector('.next')
 const imageContainer = document.querySelector('.image-container')
+const imgsEl = document.querySelectorAll('img')
 
-let currentImg = 1;
-prev.addEventListener('click',()=>{
-    currentImg++
-    updateImg()
+let timeout
+
+let currentImg = 1
+next.addEventListener('click', () => {
+  currentImg++
+  clearTimeout(timeOut)
+  updateImg(timeout)
 })
-
-function updateImg(){
-    imageContainer.style.transform= `translateX(-${currentImg-1}px)`
+updateImg()
+function updateImg() {
+  if (currentImg > imgsEl.length) {
+    currentImg = 1
+  } else if (currentImg < 1) {
+    currentImg = imgsEl.length
+  }
+  imageContainer.style.transform = `translateX(-${(currentImg - 1) * 500}px)`
+  timeout = setTimeout(() => {
+    currentImg++
+    clearTimeout(timeout)
+    updateImg()
+  }, 3000)
 }
+
+prev.addEventListener('click', () => {
+  currentImg--
+  clearTimeout(timeout)
+  updateImg()
+})
